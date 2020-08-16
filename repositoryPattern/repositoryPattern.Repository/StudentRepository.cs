9fd8ce64a3f4150ac61908fdcbe30ad94dab44f4
Ahmad Rahmani <ahmadrahmani1060@gmail.com>
2020-08-16 16:51:54 +0430

    ado.net

--- repositoryPattern/repositoryPattern.Repository/StudentRepository.cs
+++ repositoryPattern/repositoryPattern.Repository/StudentRepository.cs
@@ -4,6 +4,9 @@ using System.Linq;
 using System.Linq.Expressions;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.EntityFrameworkCore.Query;
+using System.Data;
+using System.Reflection;
+using Microsoft.Data.SqlClient;
 using repositoryPattern.Entities;
 using repositoryPattern.Data;
 
@@ -13,10 +16,51 @@ namespace repositoryPattern.Repository
     {
         private readonly ApplicationContext _dbContext;
 
-        public StudentRepository(ApplicationContext dbContext):base(dbContext)
+        public StudentRepository(ApplicationContext dbContext) : base(dbContext)
         {
             _dbContext = dbContext;
         }
+        public override IEnumerable<Student> GetAll()
+        {
+            var dt = new DataTable();
+            var connection = new SqlConnection(@"Server=.\;Database=SchoolDb;Trusted_Connection=True;MultipleActiveResultSets=true");
+            SqlCommand command = new SqlCommand("select * from students", connection);
+            command.CommandType = CommandType.Text;
+            command.Connection.Open();
+            dt.Load(command.ExecuteReader());
+            command.Connection.Close();
+            List<Student> students = new List<Student>() { };
+            students = ConvertDataTable<Student>(dt);
+            return students;
 
+            //return _dbContext.Grades.Include(i => i.Courses).ToList();
+        }
+        private static List<T> ConvertDataTable<T>(DataTable dt)
+        {
+            List<T> data = new List<T>();
+            foreach (DataRow row in dt.Rows)
+            {
+                T item = GetItem<T>(row);
+                data.Add(item);
+            }
+            return data;
+        }
+        private static T GetItem<T>(DataRow dr)
+        {
+            Type temp = typeof(T);
+            T obj = Activator.CreateInstance<T>();
+
+            foreach (DataColumn column in dr.Table.Columns)
+            {
+                foreach (PropertyInfo pro in temp.GetProperties())
+                {
+                    if (pro.Name == column.ColumnName)
+                        pro.SetValue(obj, dr[column.ColumnName], null);
+                    else
+                        continue;
+                }
+            }
+            return obj;
+        }
     }
 }
